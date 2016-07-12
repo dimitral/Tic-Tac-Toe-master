465a06a64b9f1b0c63a2438e269bff8efd9c436e
dimitral <dimitra.liva@gmail.com>
2016-07-12 10:38:15 +0300

    node

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-for (let a = 3; 3 < 100; a++){
-  console.log(a)
+// content of index.js
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
